c26f11af0c8881a4e01c3ee0086c03d4a73d3a64
Josalison <josalison.vit@gmail.com>
2026-06-15 14:17:29 -0300

    primeiro teste passando com status 200

--- test/test_api.py
+++ test/test_api.py
@@ -1,7 +1,7 @@
 import pytest
 from agents.test_agent import AgenteTestador
 
-def test_verificar_status_code():
+def test_verificar_status_code_200():
     agente = AgenteTestador()
     resposta = agente.fazer_requisicao("/posts/1")
     assert resposta.status_code == 200
