55ef66387528ffb04f56bdf2be2daa4c86a0b422
Josalison <josalison.vit@gmail.com>
2026-06-15 08:31:15 -0300

    criando requisição

---
+++ agents/test_agent.py
@@ -0,0 +1,13 @@
+import requests
+from config.settings import API_URL, timeout, HEADERS
+class AgenteTestador:
+    def __init__(self):
+        self.url = API_URL
+        self.timeout = timeout
+        self.headers = HEADERS
+
+    def requisicao(self, endpoint):
+        url_completa = self.url + endpoint
+        resposta = requests.get(url_completa, headers=self.headers, timeout= self.timeout)
+        return resposta
+    
